f6f2f92abc13b25a349228bf992d8a5ada5bf926
David Baeza <david.isaac.baeza@gmail.com>
2021-02-07 17:09:36 -0600

    Site setup and added components

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function Home(props) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md m-1">
+                   <h1>Hello World</h1>
+                   <h3>Recipe Tutorial Code will go here</h3>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+
+    render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        }
+
+        
+
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to='/home' />
+                </Switch>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Main;
